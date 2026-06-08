3ecf67f5ab607c0eb5dfd19f0b3e8716ef73a30a
masin05 <maximoflorenza8@gmail.com>
2026-06-07 22:18:35 -0300

    Initialize main function for game board setup

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "tablero.h"
+int main()
+{
+  int n;
+
+  printf("Ingrese el tamaño del tablero: ");
+  scanf("%d", &n);
+
+  srand(time(NULL));
+
+  int **tablero;
+  tablero = crearTablero(n);
+
+  agregarFichaAleatoria(tablero, n);
+  agregarFichaAleatoria(tablero, n);
+
+  char opcion;
+}
