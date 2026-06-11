0b8361f694e05bd4090c409fc17db20ed84afa56
masin05 <maximoflorenza8@gmail.com>
2026-06-11 20:45:56 -0300

    Implement stack push and pop functions

--- pila.c
+++ pila.c
@@ -8,3 +8,25 @@ int pilaVacia (Pila p){
   return p.tope == NULL;
 }
 
+void apilar(Pila *p, Tablero t){
+    Nodo *nuevo = (Nodo *)malloc(sizeof(Nodo));
+    if (nuevo == NULL){
+        printf("Error al reservar memoria\n");
+        exit(1);
+    }
+    nuevo->estado = copiarTablero(t);
+    nuevo->sig = p->tope;
+    p->tope = nuevo;
+}
+
+Tablero desapilar(Pila *p){
+    if (pilaVacia(*p)){
+        printf("La pila esta vacia\n");
+        exit(1);
+    }
+    Nodo *aux = p->tope;
+    Tablero t = aux->estado;
+    p->tope = aux->sig;
+    free(aux);
+    return t;
+}
